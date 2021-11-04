e8cfda398118db6f974759705d82f5a7bb0a2ee9
maria <ma.usachova@gmail.com>
2021-11-04 23:32:29 +0300

    клик тейлор

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function ChangeTaylor(Taylor) {
+    if (Taylor.src == "https://raw.githubusercontent.com/usachova/ITMO-WEB/main/pics/dog.jpg")
+        Taylor.src = "https://raw.githubusercontent.com/usachova/ITMO-WEB/main/pics/jaba.jpg";
+    else
+        Taylor.src = "https://raw.githubusercontent.com/usachova/ITMO-WEB/main/pics/dog.jpg";
+}
