443a4f58cebd5875e6c81982d5383d85d984b241
maria <ma.usachova@gmail.com>
2021-11-18 23:17:54 +0300

    я не знаю что нового что старого я уже ничего не знаю

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ function ChangeTaylor(Taylor) {
 }
 
 var loadTime = (function () {
-    var now = new Date().getTime();
-    var page_load_time = now - performance.timing.navigationStart;
-    return page_load_time / 1000;
+    return performance.now() / 1000;
 })();
+
+document.addEventListener("DOMContentLoaded", loadTime);
