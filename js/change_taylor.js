baa98c28deade08de2fbdefc66cc3af8af71c175
maria <ma.usachova@gmail.com>
2022-01-22 15:39:13 +0300

    new

--- js/change_taylor.js
+++ js/change_taylor.js
@@ -11,3 +11,16 @@ function ChangeTaylor(Taylor) {
     }
 
 }
+
+function ChangeTaylor2(Taylor) {
+    if (Taylor.src == "https://raw.githubusercontent.com/usachova/pictures-for-site/main/taylor2.0-1.jpeg") {
+        Taylor.src = "https://raw.githubusercontent.com/usachova/pictures-for-site/main/taylor2.0-2.jpeg";
+        Taylor.title="лапы.";
+        Taylor.alt="лапы.";
+    }
+    else {
+        Taylor.src = "https://raw.githubusercontent.com/usachova/pictures-for-site/main/taylor2.0-1.jpeg";
+        Taylor.title="мордочка.";
+        Taylor.alt="мордочка.";
+    }
+}
