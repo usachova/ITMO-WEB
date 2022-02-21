48916642a98c2367161d7b3205f3646ac5f611ea
maria <ma.usachova@gmail.com>
2022-02-21 20:56:14 +0300

    аис

--- js/change_taylor.js
+++ js/change_taylor.js
@@ -12,6 +12,7 @@ function ChangeTaylor(Taylor) {
 
 }
 
+
 function ChangeTaylor2(Taylor) {
     if (Taylor.src == "https://raw.githubusercontent.com/usachova/ITMO-WEB/main/pics/taylor2.0-1.jpeg") {
         Taylor.src = "https://raw.githubusercontent.com/usachova/ITMO-WEB/main/pics/taylor2.0-2.jpeg";
